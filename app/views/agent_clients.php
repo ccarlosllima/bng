d59e35105b880088df9bca9d732f32b82aa9e599
carlos lima <ccarlos.llima@gmail.com>
2023-06-29 13:01:01 -0300

    implementado dataTables

--- app/views/agent_clients.php
+++ app/views/agent_clients.php
@@ -3,18 +3,21 @@
 
         <!-- os meus clientes -->
         <div class="col-12 p-5 bg-white">
-    <?php 
+            <?php
 
-    // dd($data);
-?>
+            // dd($data);
+            ?>
             <div class="row">
                 <div class="col">
-                    <h5><i class="fa-solid fa-user-tie me-2"></i>Agente: <strong><?= $data['user']->name ?></strong></h5>
+                    <h5><i class="fa-solid fa-user-tie me-2"></i>Agente: <strong>
+                            <?= $data['user']->name ?>
+                        </strong></h5>
                 </div>
                 <div class="col text-end">
                     <a href="#" class="btn btn-secondary"><i class="fa-solid fa-upload me-2"></i></i>Carregar
                         ficheiro</a>
-                    <a href="?ct=Agent&mt=new_client_frm" class="btn btn-secondary"><i class="fa-solid fa-plus me-2"></i>Novo cliente</a>
+                    <a href="?ct=Agent&mt=new_client_frm" class="btn btn-secondary"><i
+                            class="fa-solid fa-plus me-2"></i>Novo cliente</a>
                 </div>
             </div>
             <hr>
@@ -23,7 +26,7 @@
                 <p class="my-5 text-center opacity-75">Não existem clientes registados.</p>
             <?php endif; ?>
 
-            <table class="table table-striped table-bordered">
+            <table class="table table-striped table-bordered" id="table_clients">
                 <thead class="table-dark">
                     <tr>
                         <th>Nome</th>
@@ -36,20 +39,32 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php  foreach ($data['clients'] as $client): ?>
-                    <tr>
-                        <td><?= $client->name ?></td>
-                        <td class="text-center"><?= $client->gender ?></td>
-                        <td class="text-center"><?=$client->birthdate ?></td>
-                        <td><?= $client->email ?></td>
-                        <td class="text-center"><?= $client->phone ?></td>
-                        <td><?= $client->interests ?></td>
-                        <td class="text-end">
-                            <a href="?ct=Agent&mt=edit_client&id=<?=aes_encrypt($client->id) ?>"><i class="fa-regular fa-pen-to-square me-2"></i>Editar</a>
-                            <span class="mx-2 opacity-50">|</span>
-                            <a href="?ct=Agent&mt=delete_client&id=<?=aes_encrypt($client->id) ?>"><i class="fa-solid fa-trash-can me-2"></i>Eliminar</a>
-                        </td>
-                    </tr>
+                    <?php foreach ($data['clients'] as $client): ?>
+                        <tr>
+                            <td>
+                                <?= $client->name ?>
+                            </td>
+                            <td class="text-center">
+                                <?= $client->gender ?>
+                            </td>
+                            <td class="text-center"><?= $client->birthdate ?></td>
+                            <td>
+                                <?= $client->email ?>
+                            </td>
+                            <td class="text-center">
+                                <?= $client->phone ?>
+                            </td>
+                            <td>
+                                <?= $client->interests ?>
+                            </td>
+                            <td class="text-end">
+                                <a href="?ct=Agent&mt=edit_client&id=<?= aes_encrypt($client->id) ?>"><i
+                                        class="fa-regular fa-pen-to-square me-2"></i>Editar</a>
+                                <span class="mx-2 opacity-50">|</span>
+                                <a href="?ct=Agent&mt=delete_client&id=<?= aes_encrypt($client->id) ?>"><i
+                                        class="fa-solid fa-trash-can me-2"></i>Eliminar</a>
+                            </td>
+                        </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
@@ -58,7 +73,7 @@
                 <div class="col">
                     <p class="mb-5">Total: <strong>[Adicionar Count]</strong></p>
                 </div>
-                <div class="col text-end">
+                <div class="col text-end mt-3">
                     <a href="#" class="btn btn-secondary">
                         <i class="fa-regular fa-file-excel me-2"></i>Exportar para XLSX
                     </a>
@@ -68,3 +83,38 @@
         </div>
     </div>
 </div>
+
+<script>
+    $(document).ready(function() {
+
+        // datatable
+        $('#table_clients').DataTable({
+            pageLength: 10,
+            pagingType: "full_numbers",
+            language: {
+                decimal: "",
+                emptyTable: "Sem dados disponíveis na tabela.",
+                info: "Mostrando _START_ até _END_ de _TOTAL_ registos",
+                infoEmpty: "Mostrando 0 até 0 de 0 registos",
+                infoFiltered: "(Filtrando _MAX_ total de registos)",
+                infoPostFix: "",
+                thousands: ",",
+                lengthMenu: "Mostrando _MENU_ registos por página.",
+                loadingRecords: "Carregando...",
+                processing: "Processando...",
+                search: "Filtrar:",
+                zeroRecords: "Nenhum registro encontrado.",
+                paginate: {
+                    first: "Primeira",
+                    last: "Última",
+                    next: "Seguinte",
+                    previous: "Anterior"
+                },
+                aria: {
+                    sortAscending: ": ative para classificar a coluna em ordem crescente.",
+                    sortDescending: ": ative para classificar a coluna em ordem decrescente."
+                }
+            }
+        });
+    })
+</script>
